098ffbb2dd6391814c46d6a4ed978938a906fde2
Vladimir-19 <v.s.solovey@gmail.com>
2024-12-23 19:57:57 -0800

    clean up

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
